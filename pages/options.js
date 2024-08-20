c2dbcd0fcafcc2ae71fb641aae32522c21d51f06
kiet.ly <kiet.ly@ifinancial.systems>
2024-08-20 15:24:01 +0700

    add UI to option page

--- pages/options.js
+++ pages/options.js
@@ -3,21 +3,33 @@ document.getElementById("save").addEventListener("click", async () => {
     const newRules = await getNewRules();
     const oldRules = await chrome.declarativeNetRequest.getDynamicRules();
     const oldRuleIds = oldRules.map((rule) => rule.id);
-
     // Use the arrays to update the dynamic rules
     await chrome.declarativeNetRequest.updateDynamicRules({
         removeRuleIds: oldRuleIds,
         addRules: newRules,
     });
-
-    console.log(oldRuleIds, newRules);
+    showSuccessMessage();
 });
 
+const showSuccessMessage = () => {
+    // Show the success message
+    const successMessage = document.getElementById("successMessage");
+    successMessage.style.display = "block";
+    successMessage.style.opacity = "1";
+
+    // Optionally, hide the message after a few seconds
+    setTimeout(() => {
+        successMessage.style.opacity = "0";
+        setTimeout(() => {
+            successMessage.style.display = "none";
+        }, 500); // Matches the transition time for a smooth fade-out
+    }, 2000); // Message is visible for 2 seconds
+};
+
 const generateRandomId = () => {
-    // Generate a random number between 1 and 1,000,000
-    // +1 shifts the range to be between 1 and 1,000,000, ensuring the id is a positive integer.
     // Math.random() generates a number between 0 (inclusive) and 1 (exclusive), like 0.123456789
     // Multiplying by 1000000 stretches this range to 0 to 999,999. Which reduces the likelihood of generating the same id on consecutive calls.
+    // +1 shifts the range to be between 1 and 1,000,000, ensuring the id is a positive integer.
     return Math.floor(Math.random() * 1000000) + 1;
 };
 
@@ -32,8 +44,7 @@ const validateUrl = (url) => {
 };
 
 const getNewRules = async () => {
-    const item1 = document.getElementById("item1").value.trim();
-    const item2 = document.getElementById("item2").value.trim();
+    const sourceUrl = document.getElementById("sourceUrl").value.trim();
     const redirectUrl = document.getElementById("redirectUrl").value.trim();
 
     if (!validateUrl(redirectUrl)) {
@@ -41,20 +52,41 @@ const getNewRules = async () => {
         return [];
     }
 
-    const blacklist = [item1, item2].filter((url) => url !== "");
-
-    return blacklist.map((x) => {
-        return {
-            id: generateRandomId(),
-            priority: 1,
-            action: {
-                type: "redirect",
-                redirect: { url: redirectUrl },
-            },
-            condition: {
-                urlFilter: x,
-                resourceTypes: ["main_frame"],
-            },
-        };
-    });
+    return {
+        id: generateRandomId(),
+        priority: 1,
+        action: {
+            type: "redirect",
+            redirect: { url: redirectUrl },
+        },
+        condition: {
+            urlFilter: sourceUrl,
+            resourceTypes: ["main_frame"],
+        },
+    };
 };
+
+const init = async () => {
+    const oldRules = await chrome.declarativeNetRequest.getDynamicRules();
+    const urls = oldRules.map((x) => x.condition.urlFilter);
+    if (urls.length === 0) {
+        return;
+    }
+    urls.forEach((x) => addUrlToList(x));
+};
+
+const addUrlToList = (url) => {
+    // Find the <ul> element in the DOM
+    const ul = document.getElementById("blacklist");
+
+    // Create a new <li> element
+    const li = document.createElement("li");
+
+    // Set the text content of the <li> to the provided URL
+    li.textContent = url;
+
+    // Append the new <li> to the <ul>
+    ul.appendChild(li);
+};
+
+init();
